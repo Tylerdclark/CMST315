a95f3e9d19ea0a0b7efb87359dfec937a622fb03
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-09-15 22:06:03 -0400

    lil work on space game

--- 3D ASTEROIDS/Assets/Scripts/PlayerController.cs
+++ 3D ASTEROIDS/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 1.0f;
+    private Rigidbody playerRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        playerRB.AddForce(Vector3.up * verticalInput * speed);
+        playerRB.AddForce(Vector3.right * horizontalInput * speed);
+
+        if (transform.position.x > 10)//This number is likely wrong
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z)
+        }//do for negative and y axis
         
     }
 }
--- 3D ASTEROIDS/Assets/Scripts/ScrollingBackground.cs
+++ 3D ASTEROIDS/Assets/Scripts/ScrollingBackground.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ScrollingBackground : MonoBehaviour
 {
+    public PlayerController player;
+    public float parallax = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +20,8 @@ public class ScrollingBackground : MonoBehaviour
         var mat = meshRenderer.material;
 
         Vector2 offset = mat.mainTextureOffset;
-        offset.x += Time.deltaTime / 10f;
+        offset.x = player.transform.position.x / player.transform.localScale.x /parallax;
+        offset.y = player.transform.position.y / player.transform.localScale.y /parallax;
 
         mat.mainTextureOffset = offset;
     }
